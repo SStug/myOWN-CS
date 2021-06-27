6615c7437c0f55e029ecc7e12a281ad78a0c2a51
SStug <86543026+SStug@users.noreply.github.com>
2021-06-27 06:12:42 +0200

    Delete water_color.lua

--- water_color.lua
+++
@@ -1,9 +0,0 @@
-function setWaterColor(command, red, green, blue)
-  setWaterColor(red, green, blue)
-  end
-addCommandHandler("setwc", setWaterColor)
-function resetWaterColor()
-  resetWaterColor()
-  end
-addCommandHandler("resetwc", resetWaterColor)
-addEventHandler("onClientResourceStop", getRootElement(), setWaterColor)
